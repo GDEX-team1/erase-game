d1fda52c5714f7913443fef31aac705f0639a8a7
kei143 <tungnet@gmail.com>
2022-05-21 13:03:47 -0400

    Adding AI & VFX for crab to Pick up Lemons and Sand Dollars

---
+++ erase-game-godot/Scenes/scripts/PickUp.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+var pickedUp = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	$PickUpSprite.play()
+	
+	if pickedUp: 
+		$PickUpVFX.show()
+		$PickUpVFX.play("PickedUp")
+		$PickUpSprite.hide()
+	if pickedUp == false:
+		$PickUpVFX.hide()	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Lemon_body_entered(body):
+	pickedUp = true
+	emit_signal("Lemon_PickedUp")
+	print("Lemon picked up: ", pickedUp)
+
+func _on_Lemon_body_exited(body):
+	pickedUp = false
+
+
+
+func _on_SandDollar_body_entered(body):
+	pickedUp = true
+	emit_signal("SandDollar_PickedUp")
+	print("SD picked up: ", pickedUp)
+	
+func _on_SandDollar_body_exited(body):
+	pickedUp = false	
