8d34412f016d4724043aa52514d17607ad3ae3c4
kei143 <tungnet@gmail.com>
2022-05-21 19:57:44 -0400

    Wall Drop Logic w/ new instance bug

--- erase-game-godot/Scenes/scripts/DragNDrop.gd
+++ erase-game-godot/Scenes/scripts/DragNDrop.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
 
-var max_Sand_Meter_Frames = 5.0  #Max number of animated frames for Sand Meter, would be nice if I can automate this
+
 export (int) var HP_Sand_Meter = 2.0 #HP per Sand Meter Frame
 export (int) var HP_Sand_Dollar = 3.0 #HP the Sand Dollars replenish for the Sand Meter
+export (int) var speed_SandWall = 60 #Speed the SandWalls travel at
+export (PackedScene) var Wall 
+
+var max_Sand_Meter_Frames = 5.0  #Max number of animated frames for Sand Meter, would be nice if I can automate this
 var currentMeterHP
 var maxMeterHP
 var wallSelected
+var new_wall 
 
-
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,23 +21,19 @@ func _ready():
 	maxMeterHP = max_Sand_Meter_Frames * HP_Sand_Meter
 	currentMeterHP = maxMeterHP
 	$SandDollar.connect("SandDollar_PickedUp",self,"pickedUp_SandDollar")
-	
-	
+	new_wall = Wall.instance()
 
 
 
 func pickedUp_SandDollar():
-	
 	currentMeterHP += HP_Sand_Dollar
-	print("HP: ", currentMeterHP,"/",maxMeterHP)
+	
 	
 func _physics_process(delta):
-	if wallSelected:
-		$Wall.position = get_global_mouse_position()
-		$Wall.show() #need to make it so infinite walls can be spawned
 	
-	#if connect("SandDollar_PickedUp",self,"_physics_process"):
-	#	currentMeterHP += HP_Sand_Dollar
+	if wallSelected == true: #and Input.is_action_just_pressed("ui_leftclick"):
+		new_wall.set_position(get_local_mouse_position())	
+		
 	
 	currentMeterHP = clamp(currentMeterHP,0,maxMeterHP)
 	updateMeterSprite()
@@ -40,23 +41,36 @@ func _physics_process(delta):
 		
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	
+	# Selecting the wall
 	if Input.is_action_just_pressed("ui_leftclick") and not wallSelected:
-		wallSelected=true
+		wallSelected = true
 		currentMeterHP -= 1
+		add_child(new_wall)
+		print("Wall Picked Up")
 		
+			
 	
 func _input(event):
+	# Cancel wall
 	if Input.is_action_just_pressed("ui_rightclick") and wallSelected:
 		wallSelected = false
+		remove_child(new_wall)
 		currentMeterHP +=1
+	
+	# Place wall
+	if Input.is_action_just_pressed("ui_leftclick") and wallSelected:
+		wallSelected = false
+		new_wall.set_position(get_local_mouse_position())
+		
+		
 	## Code used for manually adjusting Sand Meter HP
 	if Input.is_action_just_pressed("ui_up"):
 		currentMeterHP +=1
-		print("HP: ", currentMeterHP,"/",maxMeterHP)
+		
 		
 	if Input.is_action_just_pressed("ui_down"):
 		currentMeterHP -=1
-		print("HP: ", currentMeterHP,"/",maxMeterHP)
+		
 		
 	
 	
@@ -72,4 +86,5 @@ func updateMeterSprite():
 	$SandMeter_Main.frame = clamp(f-1 , 0 , maxMeterHP / HP_Sand_Meter)
 	$SandMeter_Silhouette.frame = clamp(f , 0 , maxMeterHP / HP_Sand_Meter)
 	$SandMeter_Silhouette.modulate.a = float((currentMeterHP / HP_Sand_Meter) - f)
+	print("HP: ", currentMeterHP,"/",maxMeterHP)
 		
