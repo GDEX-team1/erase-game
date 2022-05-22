b8c26706a926f9d357383be9b36f6dad8687b0db
mfaqiri <mfaqiri33@gmail.com>
2022-05-22 17:11:27 -0400

    mfaqiri-feature fixed button

--- erase-game-godot/Scenes/scripts/main.gd
+++ erase-game-godot/Scenes/scripts/main.gd
@@ -3,7 +3,7 @@ extends Node
 
 
 
-func _on_mainmenu_button_pressed() -> void:
+func _on_mainmenu_button_pressed():
 	var rc = get_tree().change_scene("res://Scenes/main/kei-main.tscn")
 
 	print(rc)
